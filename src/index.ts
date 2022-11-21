801b1d2bed65cea9167d33a01af27b6fd7692b55
LowEntropyBody <jszyyx@163.com>
2022-11-21 13:50:01 +0800

    support cors

--- src/index.ts
+++ src/index.ts
@@ -49,6 +49,14 @@ async function main() {
         mysql
     }
 
+    // Cors
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Headers', 'Authorization,X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method')
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, PUT, DELETE')
+        res.header('Allow', 'GET, POST, PATCH, OPTIONS, PUT, DELETE')
+        next();
+    });
     // Log handler
     app.use(loggingResponse);
     // API timeout handler
